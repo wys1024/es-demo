b24469083c2514ebb4730b155c101e7b5db10f9c
wys <wys9853@.com>
2025-03-22 19:19:31 +0800

    feat(core): 增加 Elasticsearch 连接功能并设置客户端参数

--- core/es_connect.go
+++ core/es_connect.go
@@ -1,14 +1,20 @@
 package core
 
-import "github.com/olivere/elastic/v7"
+import (
+	"fmt"
+	"github.com/olivere/elastic/v7"
+	"log"
+)
 
 // EsConnect 用于建立与Elasticsearch的连接。
 // 该函数不接收任何参数，也不返回任何值。
 // 它通常用于初始化Elasticsearch客户端，以便后续的搜索或索引操作。
 func EsConnect() {
-	client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"))
+	client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"), elastic.SetSniff(false))
 	if err != nil {
-		panic(err)
+		log.Fatal("Error creating the client: %s", err)
+		return
 	}
+	fmt.Println("Elasticsearch client connected", client)
 
 }
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "es-demo/core"
+
+func main() {
+	core.EsConnect()
+}
